bf7d2a1a099a5142069af9efb9e42c96f5a4f9cd
Esmatullah <esmatqadeemi12@gmail.com>
2025-10-16 07:09:35 +0430

    update

---
+++ src/main/java/com/library/library/springboot/Controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.library.library.springboot.Controller;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+public class HelloController {
+    private MessageSource messageSource;
+
+    public HelloController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
+    @GetMapping("hello")
+    public String hello(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null,"Default Message",locale);
+    }
+}
